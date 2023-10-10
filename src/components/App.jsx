26d8bda2feb63697ed715b36f559a4677a55994f
Mohitpuri Goswami <mohitgoswami379@gmail.com>
2023-10-10 08:59:36 +0530

    Keeper-part-1

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
+
+// setTimeout(() => {
+//     console.log("chutiya");
+//   }, 3000);
+
+function App(){
+    return (
+        <div>
+            <Header />
+            <Note />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import notes from "../notes";
+
+function Note(props){
+    return (
+        <div className="note">
+            <h1>This is the note title</h1>
+            <p>This is the note content</p>
+        </div>
+    );
+}
+
+export default Note;
