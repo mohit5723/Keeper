b676c75ac1a9986dac7ab06b64c642db12be019f
Mohitpuri Goswami <mohitgoswami379@gmail.com>
2023-10-10 09:29:44 +0530

    keeper-part-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,30 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes.js"
 
-// setTimeout(() => {
-//     console.log("chutiya");
-//   }, 3000);
+function createNote(context){
+    return(
+        <Note 
+            key={context.key}
+            title={context.title}
+            content={context.content}
+        />
+    )
+}
+
 
 function App(){
     return (
         <div>
             <Header />
-            <Note />
+
+            {notes.map(createNote)}
+
+            {/* <Note
+                 title={notes[0].title}
+                 content={notes[0].content}
+            /> */}
             <Footer />
         </div>
     );
